67a518aac6793b8759064b011b6ee1319edf1e88
Subhash Kumar <subhashk1508@gmail.com>
2025-05-30 02:26:56 +0530

    sized change

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ function Logo({width = '100px'}) {
   return (
     <div className='flex flexwrap-col items-center'>
     <img className='w-10 h-10' src="/logo.png" alt="" />
-    <p className='font-semibold text-xl'>BlogSpace</p>
+    <p className='font-semibold text-2xl'>BlogSpace</p>
     </div>
   )
 }
