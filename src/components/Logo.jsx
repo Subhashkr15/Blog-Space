f2dc66c8e9a4d6ceac646594da78713ddfd0d122
Subhash Kumar <subhashk1508@gmail.com>
2025-05-29 23:22:22 +0530

    extension added to components

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width = '100px'}) {
   return (
     <div className='flex flexwrap-col items-center'>
-    <img className='w-10 h-10' src="../public/logo.png" alt="" />
+    <img className='w-10 h-10' src="/logo.png" alt="" />
     <p className='font-semibold text-xl'>BlogSpace</p>
     </div>
   )
