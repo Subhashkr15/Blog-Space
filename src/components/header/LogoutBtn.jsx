d01f472d93a0700203bd5b7d3deaf51929affc7c
Subhash Kumar <subhashk1508@gmail.com>
2025-05-31 01:50:51 +0530

    colour change of logout button

--- src/components/header/LogoutBtn.jsx
+++ src/components/header/LogoutBtn.jsx
@@ -12,7 +12,7 @@ function LogoutBtn() {
     }
   return (
     <button
-    className='inline-bock px-6 py-2 duration-200 text-xl font-semibold cursor-pointer hover:text-white bg-[#edfe8c] hover:bg-gradient-to-r from-[#8a6aff] to-[#dda6ff] rounded-full'
+    className='inline-bock px-6 py-2 duration-200 text-xl font-semibold cursor-pointer hover:text-white bg-purple-300 hover:bg-gradient-to-r from-[#8a6aff] to-[#dda6ff] rounded-full'
     onClick={logoutHandler}
     >Logout</button>
   )
